0672ed63252aab1c08ec0c25f05a24a324b11a10
CNOCycle <github@marginalman.org>
2026-07-20 21:58:02 +0800

    Add planned input manifest schema

--- src/runforge/schemas/__init__.py
+++ src/runforge/schemas/__init__.py
@@ -7,6 +7,15 @@ from runforge.schemas.experiment import (
     ExperimentSchemaError,
     ExperimentStatus,
 )
+from runforge.schemas.inputs import (
+    INPUT_MANIFEST_FILE,
+    INPUT_MANIFEST_SCHEMA_VERSION,
+    INPUTS_DIRECTORY,
+    PlannedInput,
+    PlannedInputError,
+    PlannedInputManifest,
+    require_safe_relative_input_path,
+)
 from runforge.schemas.source import (
     GIT_PATCH_FILE,
     SOURCE_SCHEMA_VERSION,
@@ -19,6 +28,9 @@ from runforge.schemas.source import (
 __all__ = [
     "EXPERIMENT_SCHEMA_VERSION",
     "GIT_PATCH_FILE",
+    "INPUT_MANIFEST_FILE",
+    "INPUT_MANIFEST_SCHEMA_VERSION",
+    "INPUTS_DIRECTORY",
     "SOURCE_SCHEMA_VERSION",
     "ExperimentCommand",
     "ExperimentConfiguration",
@@ -26,5 +38,9 @@ __all__ = [
     "ExperimentStatus",
     "GitSource",
     "PinnedGitSource",
+    "PlannedInput",
+    "PlannedInputError",
+    "PlannedInputManifest",
     "SourceMetadataError",
+    "require_safe_relative_input_path",
 ]
